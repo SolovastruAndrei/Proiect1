92be5ca17aef2f2f522d63a3c3d73377096fc6a3
SolovastruAndrei <91962171+SolovastruAndrei@users.noreply.github.com>
2021-11-02 00:58:47 +0200

    Add files via upload

---
+++ PlayStation.java
@@ -0,0 +1,29 @@
+public class PlayStation {
+    private String nume;
+    private float pret;
+    public int contor;
+    public PlayStation(String nume,float pret){
+        this.nume=nume;
+        this.pret=pret;
+    }
+    public void setNume(String nume){
+        this.nume=nume;
+    }
+
+    public void setPret(float pret){
+        this.pret=pret;
+    }
+    public String getNume(){
+        return nume;
+    }
+    public float getPret(){
+        return pret;
+    }
+    public int contor(int d,Jocuri[] j){
+        if(j[d].getPlatforma().equals("PlayStation")){
+            return   contor++;
+        }
+        else
+            return contor;
+    };
+}
